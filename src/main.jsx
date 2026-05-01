6e9ca8e930deb9b9c27dee748424e09e8e64b42e
glassbible <135597927+glassbible@users.noreply.github.com>
2026-05-01 18:15:28 +0800

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import StockScreener from '../stock_screener.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <StockScreener />
+  </React.StrictMode>,
+)
